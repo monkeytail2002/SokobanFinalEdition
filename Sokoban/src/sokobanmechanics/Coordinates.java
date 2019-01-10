44e588e9d9c3488aaf943e5c690d5f89b461718c
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2019-01-10 17:18:10 +0000

    Add files via upload
    Updated mechanics package.  Level now reads the map and translates the characters in a switch/case.  Coordinates class now created.

---
+++ Sokoban/src/sokobanmechanics/Coordinates.java
@@ -0,0 +1,32 @@
+/**
+ * Angus MacDonald:  15009351
+ * Tutor: Karen MacKay
+ * Sokoban Assessment
+ * Version 1.1 10/01/19
+ */
+
+
+package sokobanmechanics;
+
+public class Coordinates {
+
+    private int x = 0;
+    private int y = 0;
+
+    public int getX() {  //gets the x position
+        return x;
+    }
+
+    public int getY() {
+        return y;  //Gets the y position
+    }
+
+    public void setX(int newX) {
+        x = newX;  //Sets the x position
+    }
+
+    public void setY(int newY) {
+        y = newY;  //Sets the y position
+    }
+
+}
--- Sokoban/src/sokobanmechanics/Level.java
+++ Sokoban/src/sokobanmechanics/Level.java
@@ -2,7 +2,7 @@
  * Angus MacDonald:  15009351
  * Tutor: Karen MacKay
  * Sokoban Assessment
- * Version 1.1 10/01/19
+ * Version 1.2 10/01/19
  */
 
 package sokobanmechanics;
@@ -11,7 +11,22 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import sokobanelements.Crate;
+import sokobanelements.Diamond;
+import sokobanelements.MapElements;
+import sokobanelements.Tile;
+import sokobanelements.Wall;
+import sokobanelements.WarehouseKeeper;
+
 public class Level {
+	
+	private MapElements[][] levelArray = new MapElements[25][15];
+	Wall wall = new Wall();
+	Coordinates Coord = new Coordinates();
+	Crate crate = new Crate();
+	Diamond diamond = new Diamond();
+	Tile tile = new Tile();
+	WarehouseKeeper wKeeper = new WarehouseKeeper();
 	
 	public Level() {
 		
@@ -31,6 +46,8 @@ public class Level {
 		FileReader reader = null;
 
         BufferedReader inputBuffer = null;
+        int x = 0;
+        int y = 0;
 
         try {
 
@@ -39,12 +56,76 @@ public class Level {
             String inputLine = inputBuffer.readLine();
             while (inputLine != null) {
             	System.out.println(inputLine);
+            	/**
+            	 * For loop and switchcase that translate the characters via @char into the positional co-ordinates and images.
+            	 */
+            	for (int i = 0; i < inputLine.length(); i++) {
+                    char mapItem = inputLine.charAt(i);
+                    switch (mapItem) {
+                        case 'X':
+                            wall.createElement(x, y);
+                            setMapElement(x,y,wall);
+                            break;
+                        case '*':
+                            //crate
+                            crate.createElement(x, y);
+                            setMapElement(x, y, crate);
+                            break;
+                        case '.':
+                            //diamond
+                            diamond.createElement(x, y);
+                            setMapElement(x, y, diamond);
+                            break;
+                        case ' ':
+                            //tile
+                            tile.createElement();
+                            setMapElement(x, y, tile);
+                            break;
+                        case '@':
+                            //warehouseKeeper
+                            wKeeper.createElement(x, y);
+                            setMapElement(wKeeper.warehouseCoords.getX(), wKeeper.warehouseCoords.getY(), wKeeper);
+                            break;
+                        default:
+                            System.out.println("Invalid character");
+                            System.exit(0);
+                    }
+                    //increments x and y to fit the files to the grid
+                    x++;
+                    if (x >= inputLine.length()) {
+                        y++;
+                        x = 0;
+                    }
+            }
             	inputLine = inputBuffer.readLine();	
-            	
             }
         } catch (Exception e) {
             	e.printStackTrace();
             }
 	}
+	
+	
+	/**
+	 * Returns the map multiarray so it can be passed into the reader object
+	 * @return
+	 */
+	public MapElements[][] getMap() {
+        return levelArray;
+    }
+	
+	/**
+	 * Sets up the multiarray so that the text files and elements can be positioned correctly on the grid.
+	 * @param x
+	 * @param y
+	 * @param mapElement
+	 */
+	public void setMapElement(int x, int y, MapElements mapElement) {
+	       levelArray[x][y] = mapElement;
+	   }
+	 
+	 
 
+	
+	
+	
 }
