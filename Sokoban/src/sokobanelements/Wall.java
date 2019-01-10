000cf4d3b78bdcd0a7341e8286c011548e32d493
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2019-01-10 16:03:30 +0000

    Add files via upload
    Additional package folder in Sokoban folder containing the elements of the program.

---
+++ Sokoban/src/sokobanelements/Wall.java
@@ -0,0 +1,38 @@
+/**
+ * Angus MacDonald:  15009351
+ * Tutor: Karen MacKay
+ * Sokoban Assessment
+ * Version 1.1 10/01/19
+ */
+
+
+package sokobanelements;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Wall extends MapElements{
+	
+	
+	/**
+	 * Object that creates the image for the wall and then sets it.  Then it will @return the ImageView.
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	
+	public ImageView wallImage() throws FileNotFoundException {
+		Image image = new Image(new FileInputStream("res/Wall.png"));
+		ImageView imageView = new ImageView(image);
+		return imageView;
+	}
+	
+	public void createElement() {
+		
+	}
+
+
+}
