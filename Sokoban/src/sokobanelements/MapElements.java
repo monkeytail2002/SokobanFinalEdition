ff6ffd8da744d4a3de0bd021238b8658e2278009
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2019-01-10 16:09:58 +0000

    Update MapElements.java
    Now with objects added and made an abstract class.

--- Sokoban/src/sokobanelements/MapElements.java
+++ Sokoban/src/sokobanelements/MapElements.java
@@ -1,5 +1,21 @@
+/**
+ * Angus MacDonald:  15009351
+ * Tutor: Karen MacKay
+ * Sokoban Assessment
+ * Version 1.1 10/01/19
+ */
+
+
 package sokobanelements;
 
-public class MapElements {
+import javafx.scene.image.ImageView;
+
+public abstract class MapElements {  //Class was made abstract to provide a base for the subclasses.
+	
+	private ImageView imageView;
+	
+	public ImageView getImage() {
+		return imageView;
+	}
 
 }
